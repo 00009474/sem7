25038d3baf94e8b9a26b5826372fd88a03ec772f
first last <student00009474@gmail.com>
2020-11-11 05:41:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+myFinalMarks = {'CSF': 75, 'FunPro': 80, 'WT': 85}
+
+modules = len(myFinalMarks)
+
+student_sum = sum(myFinalMarks.values())
+
+Average_mark = modules / student_sum  
+
+print('Your averagie mark is', Average_mark)
